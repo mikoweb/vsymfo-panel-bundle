e7b656584a8f176c8a286360cc3fc61415547beb
Rafał Mikołajun <rafal@mikoweb.pl>
2016-11-14 13:39:33 +0100

    error pages improvement

--- Controller/ExceptionController.php
+++ Controller/ExceptionController.php
@@ -12,15 +12,15 @@
 
 namespace vSymfo\Bundle\PanelBundle\Controller;
 
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\Response;
-use vSymfo\Core\Controller as CoreController;
 
 /**
  * @author Rafał Mikołajun <rafal@mikoweb.pl>
  * @package vSymfo Panel Bundle
  * @subpackage Controller
  */
-class ExceptionController extends CoreController
+class ExceptionController extends Controller
 {
     /**
      * @return Response
@@ -29,7 +29,7 @@ class ExceptionController extends CoreController
     {
         $params = $this->getParameter('ccdn_user_security.login_shield.block_pages');
 
-        return $this->renderDocument('TwigBundle:Exception:login-blocked.html.twig', [
+        return $this->render('TwigBundle:Exception:login-blocked.html.twig', [
             'max_attempts' => $params['after_attempts'],
             'block_duration' => $params['duration_in_minutes'],
         ]);
